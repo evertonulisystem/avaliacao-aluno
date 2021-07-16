ad8ac8c5cac982cb25e55ac6911dd466c8510ac9
evertonulisystem <evertonulisystem@gmail.com>
2021-07-15 21:44:30 -0300

    Criado o construtor e os Getters e os Setters

--- avaliacao-aluno/src/br/com/gotasdetecnologia/Aluno.java
+++ avaliacao-aluno/src/br/com/gotasdetecnologia/Aluno.java
@@ -1,12 +1,65 @@
 package br.com.gotasdetecnologia;
-public class Aluno {
-	String nomeAluno;
-	int idadeAluno;
-	String dataNascimento;
-	String numeroCPF;
-	String nomePai;
-	String nomeMae; 
 
+public class Aluno {
+	
+	private String nomeAluno;
+	private int idadeAluno;
+	private String dataNascimento;
+	private String numeroCPF;
+	private String nomePai;
+	private String nomeMae; 
+
+	public Aluno() {
+	}
+
+	public String getNomeAluno() {
+		return nomeAluno;
+	}
+
+	public void setNomeAluno(String nomeAluno) {
+		this.nomeAluno = nomeAluno;
+	}
+
+	public int getIdadeAluno() {
+		return idadeAluno;
+	}
+
+	public void setIdadeAluno(int idadeAluno) {
+		this.idadeAluno = idadeAluno;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNumeroCPF() {
+		return numeroCPF;
+	}
+
+	public void setNumeroCPF(String numeroCPF) {
+		this.numeroCPF = numeroCPF;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+	
 	
 
 }
