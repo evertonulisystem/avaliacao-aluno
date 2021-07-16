a4e99e656431139321134d10004171b9e34c9b94
evertonulisystem <evertonulisystem@gmail.com>
2021-07-15 23:29:36 -0300

    Saida inicial com dados dos aluno

--- avaliacao-aluno/src/br/com/gotasdetecnologia/Calculos.java
+++ avaliacao-aluno/src/br/com/gotasdetecnologia/Calculos.java
@@ -9,7 +9,7 @@ public class Calculos {
 		String nomeAluno = JOptionPane.showInputDialog("Informe o nome do aluno: ");
 		String idadeAluno = JOptionPane.showInputDialog("Qual a idade do aluno: ");
 		String dataNascimento = JOptionPane.showInputDialog("Informe a data de nascimento: ");
-		String numeroCPF = JOptionPane.showInputDialog("Informe a data de nascimento: ");
+		String numeroCPF = JOptionPane.showInputDialog("Informe o numero do CPF: ");
 		String nomePai = JOptionPane.showInputDialog("Informe o nome do pai do aluno: ");
 		String nomeMae = JOptionPane.showInputDialog("Informe o nome da mae do aluno: ");
 		
@@ -21,6 +21,13 @@ public class Calculos {
 		aluno1.setNumeroCPF(numeroCPF);
 		aluno1.setNomePai(nomePai);
 		aluno1.setNomeMae(nomeMae);
+		
+		//Saida Inicial com dados do aluno
+		System.out.println("Nome do aluno: " + aluno1.getNomeAluno());
+		System.out.println("Idade do aluno: " + aluno1.getIdadeAluno());
+		System.out.println("Nascimento do aluno: " + aluno1.getDataNascimento());
+		System.out.println("Pai do aluno: " + aluno1.getNomePai());
+		System.out.println("Mãe do Aluno: " + aluno1.getNomeMae());
 		
 		
 		
