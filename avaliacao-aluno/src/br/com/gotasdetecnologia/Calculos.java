167bc2255b0de16a12f183307f240a66609c86f5
evertonulisystem <evertonulisystem@gmail.com>
2021-07-15 23:07:54 -0300

    Criado solicitações de dados pessoais

---
+++ avaliacao-aluno/src/br/com/gotasdetecnologia/Calculos.java
@@ -0,0 +1,18 @@
+package br.com.gotasdetecnologia;
+
+import javax.swing.JOptionPane;
+
+public class Calculos {
+
+	public static void main(String[] args) {
+		
+		String nomeAluno = JOptionPane.showInputDialog("Informe o nome do aluno: ");
+		String idadeAluno = JOptionPane.showInputDialog("Qual a idade do aluno: ");
+		String dataNascimento = JOptionPane.showInputDialog("Informe a data de nascimento: ");
+		String numeroCPF = JOptionPane.showInputDialog("Informe a data de nascimento: ");
+		String nomePai = JOptionPane.showInputDialog("Informe o nome do pai do aluno: ");
+		String nomeMae = JOptionPane.showInputDialog("Informe o nome da mae do aluno: ");
+		
+	}
+
+}
